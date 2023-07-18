e18bfc94be15f6f123268e2e5b72948340c2a870
shaurair <shaurair@gmail.com>
2023-07-18 14:01:35 +0800

    modify coding style

--- Week3/task2.py
+++ Week3/task2.py
@@ -9,7 +9,7 @@ import csv
 # get article post time
 def GetArticleTime(url):
 	request = req.Request(url, headers = {
-	"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36"
+		"User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36"
 	})
 	with req.urlopen(request) as response:
 		data = response.read().decode("utf-8")
