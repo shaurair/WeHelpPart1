ee2a4dcc1c1286a8c75da549fe1d22b304ba93ed
shaurair <shaurair@gmail.com>
2023-07-25 13:21:04 +0800

    add assignment 4

---
+++ Week4/app.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import render_template
+from flask import session
+
+app=Flask(__name__, static_folder = "public", static_url_path = "/")
+app.secret_key = "ant string but secret"
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+
+@app.route("/signin", methods = ["POST"])
+def signin():
+	Username = request.form["Username"]
+	Password = request.form["Password"]
+	if Username == "test" and Password == "test":
+		# Record user status
+		session["SIGNED-IN"] = True
+		return redirect("/member")
+	else:
+		# Clear user status
+		session["SIGNED-IN"] = False
+
+		# Check error type
+		if Username == "" or Password == "":
+			return redirect("/error?message=Please enter username and password")
+		else:
+			return redirect("/error?message=Username or password is not correct")
+
+@app.route("/signout")
+def signout():
+	session["SIGNED-IN"] = False
+	return redirect("/")
+
+@app.route("/member")
+def member():
+	# Check user status
+	if "SIGNED-IN" in session:
+		if session["SIGNED-IN"]:
+			return render_template("member.html")
+	
+	return redirect("/")
+
+@app.route("/error")
+def error():
+	ErrMsg = request.args.get("message","")
+	return render_template("error.html", PageTitle = "Login failed.",Msg = ErrMsg)
+
+app.run(port = 3000)
